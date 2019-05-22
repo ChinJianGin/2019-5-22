bb0b33fb399df4bbe730a7360f2d99744eea0b16
ChinJianGin <48975062+ChinJianGin@users.noreply.github.com>
2019-05-22 11:26:47 +0800

    Add files via upload

---
+++ sketch_may22a/sketch_may22a.ino
@@ -0,0 +1,38 @@
+const int analogPin=A0;
+const int ledCount=10;
+int ledPins[]={
+  2,3,4,5,6,7,8,9,10,11
+  };
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(int thisLed=0;thisLed<ledCount;thisLed++){
+    pinMode(ledPins[thisLed],OUTPUT);
+    }
+}
+
+void loop() {
+int aRead=analogRead(A0);
+
+Serial.println(aRead);
+
+  int sensorReading = analogRead(analogPin);
+  if(sensorReading<450){
+    sensorReading=450;
+  }else if(sensorReading>800){
+    sensorReading=800;
+    }
+  int ledLevel = map(sensorReading, 450, 800, 0, ledCount);
+
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    
+    if (thisLed < ledLevel) {
+      digitalWrite(ledPins[thisLed], HIGH);
+    }
+   
+    else {
+      digitalWrite(ledPins[thisLed], LOW);
+    }
+delay(1);
+  }
+}
